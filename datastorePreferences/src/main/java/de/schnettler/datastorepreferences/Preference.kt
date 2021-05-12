fa3fc9147418ea17007f408f23f74d76cad881ff
Sean Proctor <sproctor@gmail.com>
2021-05-12 10:24:36 -0400

    Remove unused composable function with summary not specified in PreferenceItem

--- datastorePreferences/src/main/java/de/schnettler/datastorepreferences/Preference.kt
+++ datastorePreferences/src/main/java/de/schnettler/datastorepreferences/Preference.kt
@@ -1,37 +1,18 @@
 package de.schnettler.datastorepreferences
 
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 
-
 @ExperimentalMaterialApi
 @Composable
 fun Preference(
     item: PreferenceItem<*>,
-    summary: String? = null,
-    onClick: () -> Unit = { },
-    trailing: @Composable (() -> Unit)? = null
-) {
-    Preference(
-        item = item,
-        summary = { Text(text = summary ?: item.summary) },
-        onClick = onClick,
-        trailing = trailing
-    )
-}
-
-@ExperimentalMaterialApi
-@Composable
-fun Preference(
-    item: PreferenceItem<*>,
-    summary: @Composable () -> Unit,
     onClick: () -> Unit = { },
     trailing: @Composable (() -> Unit)? = null
 ) {
     de.schnettler.composepreferences.Preference(
         title = item.title,
-        summary = summary,
+        summary = item.summary,
         singleLineTitle = item.singleLineTitle,
         icon = item.icon,
         enabled = item.enabled,
