f81238d7db652d3eeba0b8ddf81963646f50d308
Sean Proctor <sproctor@gmail.com>
2022-10-21 09:32:27 -0400

    update to compose 1.2.0, release as 0.14.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,9 @@
-
 rootProject.name = "ComposePreferences"
+
 include(":app")
 include(":compose-preferences")
 include(":compose-preferences-settings")
 
 plugins {
-    id("de.fayard.refreshVersions") version "0.40.2"
+    id("de.fayard.refreshVersions") version "0.50.2"
 }
