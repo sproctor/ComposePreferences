af07850351fbef0f70ca677d067e02360d68bc80
Sean Proctor <sproctor@gmail.com>
2023-04-14 08:26:17 -0400

    Don't have text entry fill max width

--- compose-preferences/src/commonMain/kotlin/com/github/sproctor/composepreferences/EditTextPreference.kt
+++ compose-preferences/src/commonMain/kotlin/com/github/sproctor/composepreferences/EditTextPreference.kt
@@ -1,7 +1,6 @@
 package com.github.sproctor.composepreferences
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
@@ -9,7 +8,12 @@ import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
@@ -95,7 +99,6 @@ private fun EditTextDialog(
         var transformPassword by remember(isPassword) { mutableStateOf(isPassword) }
         TextField(
             modifier = Modifier
-                .fillMaxWidth()
                 .focusRequester(focusRequester),
             value = textValue,
             onValueChange = { textValue = it },
