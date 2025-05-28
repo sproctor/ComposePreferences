16e562ea99a6779e2244545277cdd06addd897b2
Sean Proctor <sproctor@gmail.com>
2025-05-28 16:11:24 -0400

    fix js demo

--- app/src/jsMain/kotlin/com/github/sproctor/composepreferences/demo/main.kt
+++ app/src/jsMain/kotlin/com/github/sproctor/composepreferences/demo/main.kt
@@ -1,16 +1,17 @@
 package com.github.sproctor.composepreferences.demo
 
-import androidx.compose.ui.window.Window
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.CanvasBasedWindow
 import com.russhwolf.settings.ExperimentalSettingsApi
 import com.russhwolf.settings.StorageSettings
 import com.russhwolf.settings.coroutines.toSuspendSettings
 import org.jetbrains.skiko.wasm.onWasmReady
 
-@OptIn(ExperimentalSettingsApi::class)
+@OptIn(ExperimentalSettingsApi::class, ExperimentalComposeUiApi::class)
 fun main() {
     onWasmReady {
         val settings = StorageSettings()
-        Window("Compose Preferences") {
+        CanvasBasedWindow("Compose Preferences") {
             DemoContent(settings.toSuspendSettings())
         }
     }
